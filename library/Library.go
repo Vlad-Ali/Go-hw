e3d2d964080bb3a551b3286d1eecf9aff1163889
Vlad_Ali <marknext592@gmail.com>
2025-09-21 12:39:17 +0300

    Changed field

--- library/Library.go
+++ library/Library.go
@@ -15,11 +15,11 @@ type LibraryOperations interface {
 type Library struct {
 	storage     Storage
 	titleIds    map[string]int
-	IDGenerator func(string) int
+	idGenerator func(string) int
 }
 
 func NewLibrary(storage Storage, generator func(string) int) *Library {
-	return &Library{storage: storage, titleIds: make(map[string]int), IDGenerator: generator}
+	return &Library{storage: storage, titleIds: make(map[string]int), idGenerator: generator}
 }
 
 func getBookFullName(title string, author string) string {
@@ -28,7 +28,7 @@ func getBookFullName(title string, author string) string {
 
 func (l *Library) AddBook(title string, author string) error {
 	fullName := getBookFullName(title, author)
-	bookId := l.IDGenerator(fullName)
+	bookId := l.idGenerator(fullName)
 	newBook := NewBook(title, author, bookId)
 	err := l.storage.add(newBook)
 	if err != nil {
@@ -56,7 +56,7 @@ func (l *Library) SetStorage(storage Storage) {
 }
 
 func (l *Library) SetIDGenerator(generator func(string) int) {
-	l.IDGenerator = generator
+	l.idGenerator = generator
 	currentBooks := l.storage.getAll()
 	for _, book := range currentBooks {
 		err := l.storage.remove(book.id)
@@ -66,7 +66,7 @@ func (l *Library) SetIDGenerator(generator func(string) int) {
 	}
 	for _, book := range currentBooks {
 		fullName := getBookFullName(book.title, book.author)
-		newBookId := l.IDGenerator(fullName)
+		newBookId := l.idGenerator(fullName)
 		book.id = newBookId
 		err := l.storage.add(&book)
 		if err != nil {
